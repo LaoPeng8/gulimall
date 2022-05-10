63be0c718cfb6a1dd0727629463ff450b5e980d7
LaoPeng <2779824672@qq.com>
2022-05-10 14:04:57 +0800

    测试nacos作为 配置中心的, 命名空间, 分组, 读取多个配置文件

--- gulimall-coupon/src/main/java/org/pjj/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/org/pjj/gulimall/coupon/GulimallCouponApplication.java
@@ -18,7 +18,30 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  *          @Value("${配置名}") //获取配置的值
  *      注意: 配置中心 与 本地properties同时配置 同一个配置, 配置中心优先级高
  *
- *  2.
+ *  2. 细节
+ *      1) 命名空间: 配置隔离 (重点)
+ *          默认 public(保留空间); 默认新增的所有配置都在public空间.
+ *          开发 dev, 测试 test, 生产 prop 三个命名空间, 来做环境隔离 各自的配置放在各种的命名空间中
+ *          注意: 需要在bootstrap.properties中配置 使用哪个命名空间下的配置 spring.cloud.nacos.config.namespace=命名空间ID
+ *
+ *          每一个微服务之间互相隔离配置, 每一个微服务都创建自己的命名空间, 只加载自己命名空间下的所有配置
+ *
+ *      2) 配置集: 所有配置的集合
+ *
+ *      3) 配置集ID: 类似配置文件名
+ *          Data ID: 类似文件名
+ *
+ *      4) 配置分组: (重点)
+ *          默认所有的配置集都属于 DEFAULT_GROUP; 这个配置分组
+ *          1111, 618, 1212
+ *      每个微服务创建自己的命名空间, 使用配置分组区分环境 dev, test, prop
+ *
+ *  3. 同时加载多个配置集
+ *      1) 微服务任何配置信息, 任何配置文件都可以放在配置中心
+ *      2) 只需要在bootstrap.properties说明加载配置中心哪些配置文件即可
+ *      3) @Value, @ConfigurationProperties ...
+ *      以前SpringBoot任何方法从配置文件中获取值的方法, 都能使用. (配置中心 与 本地properties同时配置 同一个配置, 配置中心优先级高)
+ *
  */
 @SpringBootApplication
 @EnableDiscoveryClient //开始nacos服务注册与发现
