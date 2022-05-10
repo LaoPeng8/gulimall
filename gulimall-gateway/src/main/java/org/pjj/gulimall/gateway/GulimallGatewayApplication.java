4b2a199d2d3cdd1e6fc7f3c1d86cbd60078563f8
LaoPeng <2779824672@qq.com>
2022-05-10 14:05:36 +0800

    测试gateway网关

---
+++ gulimall-gateway/src/main/java/org/pjj/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,16 @@
+package org.pjj.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient // 开启 nacos 服务注册 与 发现
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //启动不自动配置数据库
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
