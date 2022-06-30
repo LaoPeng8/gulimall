7ae27a7a284b0f1c20b05f261a43dc02b0a71717
LaoPeng <2779824672@qq.com>
2022-06-30 13:35:39 +0800

    sku检索(带条件的分页查询)

--- gulimall-product/src/main/java/org/pjj/gulimall/product/service/SkuInfoService.java
+++ gulimall-product/src/main/java/org/pjj/gulimall/product/service/SkuInfoService.java
@@ -18,5 +18,7 @@ import java.util.Map;
 public interface SkuInfoService extends IService<SkuInfoEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    PageUtils queryPageByCondition(Map<String, Object> params);
 }
 
--- gulimall-product/src/main/java/org/pjj/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/org/pjj/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,6 +1,7 @@
 package org.pjj.gulimall.product.service.impl;
 
 import org.pjj.common.exception.GulimallException;
+import org.pjj.gulimall.product.entity.SpuInfoEntity;
 import org.pjj.gulimall.product.entity.vo.Skus;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import org.pjj.common.utils.Query;
 import org.pjj.gulimall.product.dao.SkuInfoDao;
 import org.pjj.gulimall.product.entity.SkuInfoEntity;
 import org.pjj.gulimall.product.service.SkuInfoService;
+import org.springframework.util.StringUtils;
 
 
 @Service("skuInfoService")
@@ -27,6 +29,43 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
                 new Query<SkuInfoEntity>().getPage(params),
                 new QueryWrapper<SkuInfoEntity>()
         );
+
+        return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> spuInfoEntityQueryWrapper = new QueryWrapper<>();
+
+        String catelogId = (String) params.get("catelogId");
+        if(!StringUtils.isEmpty(catelogId) && !catelogId.equals("0")) {
+            spuInfoEntityQueryWrapper.eq("catelog_id", catelogId);
+        }
+
+        String brandId = (String) params.get("brandId");
+        if(!StringUtils.isEmpty(brandId) && !brandId.equals("0")) {
+            spuInfoEntityQueryWrapper.eq("brand_id", brandId);
+        }
+
+        String min = (String) params.get("min");
+        String max = (String) params.get("max");
+        if(!StringUtils.isEmpty(min) && !min.equals("0")) {
+            spuInfoEntityQueryWrapper.ge("price", min);
+        }
+        if(!StringUtils.isEmpty(max) && !max.equals("0")) {
+            spuInfoEntityQueryWrapper.le("price", max);
+        }
+
+        String key = (String) params.get("key");
+        if(!StringUtils.isEmpty(key)) {
+            spuInfoEntityQueryWrapper.and((wrapper) -> {
+                wrapper.like("sku_id", key).or().like("sku_name", key);
+            });
+        }
+
+        //该getPage中封装了 page对象分页current与limit, 还封装了排序sidx排序字段, order排序方式 desc/asc
+        IPage<SkuInfoEntity> page = new Query<SkuInfoEntity>().getPage(params);
+        this.page(page, spuInfoEntityQueryWrapper);
 
         return new PageUtils(page);
     }
